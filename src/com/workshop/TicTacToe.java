72fbbce210493f4b141c7c65f35d52041d766d37
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-07 09:14:15 +0530

    [Shubham]Added: Created TictacToe board

--- src/com/workshop/TicTacToe.java
+++ src/com/workshop/TicTacToe.java
@@ -1,7 +1,15 @@
 package com.workshop;
 
 public class TicTacToe {
-	public static void main(String[] args) {
-		System.out.println("Welcome");
+	public int[] CreateBoard() {
+		int board[] = new int[10];
+		return board;
 	}
+
+	public void DisplayBoard(int board[]) {
+		for (int i = 1; i < board.length; i++) {
+			System.out.println(board[i]);
+		}
+	}
+
 }
