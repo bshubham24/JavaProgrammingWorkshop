3c1084bfb7440d5f90c4be5360c0e722a1f4582a
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-07 12:57:37 +0530

    [Shubham]Added: Toss feature

--- src/com/workshop/Executor.java
+++ src/com/workshop/Executor.java
@@ -11,31 +11,36 @@ public class Executor {
 		System.out.println("Welcome");
 		TicTacToe tictactoe = new TicTacToe();
 		char board[] = tictactoe.CreateBoard();
-		while (choice != 2) {
-			user = tictactoe.TakeInput();
-			user = Character.toUpperCase(user);
-			if (user == 'X') {
-				computer = 'O';
-			} else
-				computer = 'X';
-			System.out.println("User: " + user);
-			System.out.println("Computer: " + computer);
+		char toss = tictactoe.Toss();
+		if (toss == 'H') {
+			System.out.println("User won the toss, hence plays first");
+			while (choice != 2) {
+				user = tictactoe.TakeInput();
+				user = Character.toUpperCase(user);
+				if (user == 'X') {
+					computer = 'O';
+				} else
+					computer = 'X';
+				System.out.println("User: " + user);
+				System.out.println("Computer: " + computer);
 
-			System.out.println("Enter 1 for choosing position and enter 2 for exit");
-			choice = Integer.parseInt(sc.nextLine());
-			if (choice == 1) {
-				position = tictactoe.ChoosePosition(board);
-				if (position != 0) {
-					board[position] = user;
-				}
-				tictactoe.ShowBoard(board);
+				System.out.println("Enter 1 for choosing position and enter 2 for exit");
+				choice = Integer.parseInt(sc.nextLine());
+				if (choice == 1) {
+					position = tictactoe.ChoosePosition(board);
+					if (position != 0) {
+						board[position] = user;
+					}
+					tictactoe.ShowBoard(board);
 
-			} else if (choice == 2) {
-				break;
-			} else
-				System.out.println("Enter the correct choice");
+				} else if (choice == 2) {
+					break;
+				} else
+					System.out.println("Enter the correct choice");
 
-		}
+			}
+		} else if (toss == 'T')
+			System.out.println("Computer won the toss, hence plays first");
 
 	}
 }
