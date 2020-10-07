faa711c75950365b261bdcca8124e8ff9b2d6134
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-07 09:42:47 +0530

    [Shubham]Added: Ability to take input from user

--- src/com/workshop/Executor.java
+++ src/com/workshop/Executor.java
@@ -1,10 +1,15 @@
 package com.workshop;
 
+import java.util.Scanner;
+
 public class Executor {
 	public static void main(String[] args) {
+		char User, Computer;
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome");
-		TicTacToe ticTacToe = new TicTacToe();
-		int board[] = ticTacToe.CreateBoard();
-		ticTacToe.DisplayBoard(board);
+		TicTacToe tictactoe = new TicTacToe();
+		char board[] = tictactoe.CreateBoard();
+		tictactoe.TakeInput();
+		tictactoe.DisplayBoard(board);
 	}
 }
--- src/com/workshop/TicTacToe.java
+++ src/com/workshop/TicTacToe.java
@@ -1,12 +1,24 @@
 package com.workshop;
 
+import java.util.Scanner;
+
 public class TicTacToe {
-	public int[] CreateBoard() {
-		int board[] = new int[10];
+	public char[] CreateBoard() {
+		char board[] = new char[10];
+		for (int i = 1; i < board.length; i++) {
+			board[i] = 'z';
+		}
 		return board;
 	}
 
-	public void DisplayBoard(int board[]) {
+	public char TakeInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose X or O");
+		char input = sc.next().charAt(0);
+		return input;
+	}
+
+	public void DisplayBoard(char board[]) {
 		for (int i = 1; i < board.length; i++) {
 			System.out.println(board[i]);
 		}
