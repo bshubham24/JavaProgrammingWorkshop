ab191e821948d53ef7f3f709cfdadbbaed395f67
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-07 10:00:25 +0530

    [Shubham]Added: Ability to take input from user

--- src/com/workshop/Executor.java
+++ src/com/workshop/Executor.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Executor {
 	public static void main(String[] args) {
-		char User, Computer;
+		char user, computer;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome");
 		TicTacToe tictactoe = new TicTacToe();
 		char board[] = tictactoe.CreateBoard();
-		tictactoe.TakeInput();
-		tictactoe.DisplayBoard(board);
+		user = tictactoe.TakeInput();
+		user = Character.toUpperCase(user);
+		if (user == 'X') {
+			computer = 'O';
+		} else
+			computer = 'X';
+		System.out.println("User: " + user);
+		System.out.println("Computer: " + computer);
+
 	}
 }
