e93dbddcc44fab34cfbb313b22a2f63fd6ec4124
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-07 12:27:54 +0530

    [Shubham]Refactored: Ability to take choice till exit

--- src/com/workshop/Executor.java
+++ src/com/workshop/Executor.java
@@ -6,23 +6,36 @@ public class Executor {
 	public static void main(String[] args) {
 		char user, computer;
 		int position;
+		int choice = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome");
 		TicTacToe tictactoe = new TicTacToe();
 		char board[] = tictactoe.CreateBoard();
-		user = tictactoe.TakeInput();
-		user = Character.toUpperCase(user);
-		if (user == 'X') {
-			computer = 'O';
-		} else
-			computer = 'X';
-		System.out.println("User: " + user);
-		System.out.println("Computer: " + computer);
-		position = tictactoe.ChoosePosition(board);
-		if (position != 0) {
-			board[position] = user;
+		while (choice != 2) {
+			user = tictactoe.TakeInput();
+			user = Character.toUpperCase(user);
+			if (user == 'X') {
+				computer = 'O';
+			} else
+				computer = 'X';
+			System.out.println("User: " + user);
+			System.out.println("Computer: " + computer);
+
+			System.out.println("Enter 1 for choosing position and enter 2 for exit");
+			choice = Integer.parseInt(sc.nextLine());
+			if (choice == 1) {
+				position = tictactoe.ChoosePosition(board);
+				if (position != 0) {
+					board[position] = user;
+				}
+				tictactoe.ShowBoard(board);
+
+			} else if (choice == 2) {
+				break;
+			} else
+				System.out.println("Enter the correct choice");
+
 		}
-		tictactoe.ShowBoard(board);
 
 	}
 }
