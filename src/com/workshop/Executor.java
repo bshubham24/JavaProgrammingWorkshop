74be5c4cdc4bb29b5cf4425534f05f63e9d5104c
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-07 12:05:00 +0530

    [Shubham]Added: Ability to fill the positon based on choice

--- src/com/workshop/Executor.java
+++ src/com/workshop/Executor.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Executor {
 	public static void main(String[] args) {
 		char user, computer;
+		int position;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome");
 		TicTacToe tictactoe = new TicTacToe();
@@ -17,7 +18,10 @@ public class Executor {
 			computer = 'X';
 		System.out.println("User: " + user);
 		System.out.println("Computer: " + computer);
-
+		position = tictactoe.ChoosePosition(board);
+		if (position != 0) {
+			board[position] = user;
+		}
 		tictactoe.ShowBoard(board);
 
 	}
